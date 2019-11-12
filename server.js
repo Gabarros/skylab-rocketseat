4423d9f4c6740c23b03cc7b3e548e00069101540
Gabarros <gabarros17@gmail.com>
2019-11-11 23:31:20 -0300

    Add Git ignore

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+//Iniciando o App
 const app = express();
 
+//Iniciando o DB
+mongoose.connect('mongodb+srv://desenvolvedor:desenvolvimento123@cluster0-gedzb.mongodb.net/test?retryWrites=true&w=majority')
 
+// Iniciando Rotas
 app.get('/', (req, res)=>{
     res.send("Rodando!");
 });
